6ed2e892c9fbd406deb6fd349e6d484ec1e93eea
scan.pratik@gmai.com <scan.pratik@gmai.com>
2020-01-07 13:10:39 +0530

    P-1 ADT structure added with abstract operations

--- A1_PS6_LB_DSAD_GROUP126.py
+++ A1_PS6_LB_DSAD_GROUP126.py
@@ -1,63 +1,28 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "class bookNode:\n",
-    "    def __init__(self, bkId , availCount):\n",
-    "        self.bookId = bkId\n",
-    "        self.availCount = availCount\n",
-    "        self.checkoutCount = 0\n",
-    "        self.left = None\n",
-    "        self.right = None\n",
-    "    \n",
-    "    def readBookList(self, bkID, availCount):\n",
-    "        pass\n",
-    "    \n",
-    "    def chkInChkOut(self, bkID, inOut):\n",
-    "        pass\n",
-    "    \n",
-    "    def getTopBooks(self, bkNode):\n",
-    "        pass\n",
-    "    \n",
-    "    def notIssued(self, bkNode):\n",
-    "        pass\n",
-    "    \n",
-    "    def findBook(self, eNode, bkID):\n",
-    "        pass\n",
-    "    \n",
-    "    def stockOut(self, eNode):\n",
-    "        pass\n",
-    "    \n",
-    "    def printBooks(self, bkNode):\n",
-    "        pass\n",
-    "    \n",
-    "    "
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+class bookNode:
+    def __init__(self, bkId , availCount):
+        self.bookId = bkId
+        self.availCount = availCount
+        self.checkoutCount = 0
+        self.left = None
+        self.right = None
+    
+    def readBookList(self, bkID, availCount):
+        pass
+    
+    def chkInChkOut(self, bkID, inOut):
+        pass
+    
+    def getTopBooks(self, bkNode):
+        pass
+    
+    def notIssued(self, bkNode):
+        pass
+    
+    def findBook(self, eNode, bkID):
+        pass
+    
+    def stockOut(self, eNode):
+        pass
+    
+    def printBooks(self, bkNode):
+        pass
