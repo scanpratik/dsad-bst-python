e21c6d4db275989a7df0f872d76547e57ec3d2c5
Kamran Ali <kamranalinitb@gmail.com>
2020-01-09 00:32:44 +0530

    Removed Trim and printing tree

--- A1_PS6_LB_DSAD_GROUP126.py
+++ A1_PS6_LB_DSAD_GROUP126.py
@@ -3,8 +3,9 @@ from bookNode import bookNode
 def readBookList(path):
     inputFile = open(path,'r')
     lines = inputFile.readlines()
+    inputFile.close()
     for line in lines:
-        bookEntry = line.trim().split(', ')
+        bookEntry = line.split(', ')
         bookId = bookEntry[0]
         count = bookEntry[1]
         books._readBookList(bookId , count)
@@ -13,3 +14,4 @@ def readBookList(path):
 
 books = bookNode(0,0)
 readBookList("inputPS6.txt")
+books.PrintTree()
